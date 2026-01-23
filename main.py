328fa2588a3b97ad4da3dc5d2810cfb120bdbf9e
muriki vaishnav <136063328+vaishpaaa@users.noreply.github.com>
2026-01-23 21:37:08 +0530

    Added admin endpoint

--- main.py
+++ main.py
@@ -135,8 +135,6 @@ def get_profile(email: str):
 def get_all_orders():
     try:
         # Fetch all orders, newest first
-        # We also want user details (address/phone), but for now let's just get the orders
-        # In a real app, we would join tables. Here we will keep it simple.
         response = supabase.table("orders").select("*").order("created_at", desc=True).execute()
         return response.data
     except Exception as e:
